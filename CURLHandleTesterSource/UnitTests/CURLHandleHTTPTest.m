c75204e5890f6d5fd07f1a935c65dbea0a195afa
Ben Pettit <benpettit@digimulti.com>
2013-02-21 23:52:04 +1100

    Got the first test passing successfully.

--- CURLHandleTesterSource/UnitTests/CURLHandleHTTPTest.m
+++ CURLHandleTesterSource/UnitTests/CURLHandleHTTPTest.m
@@ -8,6 +8,7 @@
 
 #import "CURLHandleHTTPTest.h"
 #import <CURLHandle/CURLProtocol.h>
+#import <CommonCrypto/CommonDigest.h>
 
 
 #pragma mark - Private Interface
@@ -50,7 +51,7 @@ NSInteger const SECONDS_WAIT_FOR_CONNECTION_TO_COMPLETE = (NSInteger)0.25;
     [request setShouldUseCurlHandle:YES];
     self.connection = [NSURLConnection connectionWithRequest:request delegate:self];
     
-    while (NO==self.isFinished && NO==self.error) {
+    while (NO==self.isFinished && nil==self.error) {
 		[[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:SECONDS_WAIT_FOR_CONNECTION_TO_COMPLETE]];
 	}
     
@@ -62,6 +63,10 @@ NSInteger const SECONDS_WAIT_FOR_CONNECTION_TO_COMPLETE = (NSInteger)0.25;
     
     [request setShouldUseCurlHandle:NO];
     [self getFileFromSiteWithNSURLConnection:request destination:self.vanillaConnectionDestinationPath];
+    
+    NSString *curlDestChecksum = [self getMD5FromFile:self.curlHandleDestinationPath];
+    NSString *vanillaDestChecksum = [self getMD5FromFile:self.vanillaConnectionDestinationPath];
+    GHAssertEqualStrings(vanillaDestChecksum, curlDestChecksum, @"The md5 checksum of the curl destination file does not match the vanilla destination file.");
 }
 
 
@@ -82,6 +87,7 @@ NSInteger const SECONDS_WAIT_FOR_CONNECTION_TO_COMPLETE = (NSInteger)0.25;
     self.error = error;
 }
 
+
 #pragma mark Misc
 
 - (void)getFileFromSiteWithNSURLConnection:(NSURLRequest *)info destination:(NSString *)destination
@@ -90,8 +96,10 @@ NSInteger const SECONDS_WAIT_FOR_CONNECTION_TO_COMPLETE = (NSInteger)0.25;
     [self.output open];
     
     NSURLConnection *request = [[[NSURLConnection alloc] initWithRequest:info delegate:self] autorelease];
+    self.isFinished=NO;
+    self.error=nil;
     [request start];//just to remove warning about it being unused... :/
-    while (NO==self.isFinished || NO==self.error) {
+    while (NO==self.isFinished && nil==self.error) {
 		[[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:SECONDS_WAIT_FOR_CONNECTION_TO_COMPLETE]];
 	}
     GHAssertNil(self.error, @"Error occured whilst running the vanilla NSURLConnection: %@",self.error.description);
@@ -99,18 +107,40 @@ NSInteger const SECONDS_WAIT_FOR_CONNECTION_TO_COMPLETE = (NSInteger)0.25;
     [self.output close];
 }
 
+- (NSString *)getMD5FromFile:(NSString *)pathToFile {
+    unsigned char outputData[CC_MD5_DIGEST_LENGTH];
+	
+    NSData *inputData = [[NSData alloc] initWithContentsOfFile:pathToFile];
+    CC_MD5([inputData bytes], (CC_LONG)[inputData length], outputData);
+    [inputData release];
+	
+    NSMutableString *hash = [[NSMutableString new] autorelease];
+	{
+    NSUInteger i = 0;
+    for (; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [hash appendFormat:@"%02x", outputData[i]];
+    }
+	}
+	
+    return hash;
+}
+
 - (id)init
 {
     self = [super init];
     
     if (nil!=self) {
+        NSError *error;
         
         NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-        const NSString *DOCUMENT_BASE = [paths objectAtIndex:0];
+        NSString * const DOCUMENT_BASE = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"CURLHandleTests"];
+        [[NSFileManager defaultManager] createDirectoryAtPath:DOCUMENT_BASE withIntermediateDirectories:NO attributes:nil error:&error];
         
-        _CURLHandle_Destination = [DOCUMENT_BASE stringByAppendingPathComponent:@"CURLHandleTests/CURLHandle_Destination"];
+        _CURLHandleDestinationDirectory = [DOCUMENT_BASE stringByAppendingPathComponent:@"CURLHandle_Destination"];
+        [[NSFileManager defaultManager] createDirectoryAtPath:self.CURLHandleDestinationDirectory withIntermediateDirectories:NO attributes:nil error:&error];
         
-        _NSURLConnection_Destination = [DOCUMENT_BASE stringByAppendingPathComponent:@"CURLHandleTests/NSURLConnection_Destination"];
+        _VanillaConnectionDestinationDirectory = [DOCUMENT_BASE stringByAppendingPathComponent:@"NSURLConnection_Destination"];
+        [[NSFileManager defaultManager] createDirectoryAtPath:self.VanillaConnectionDestinationDirectory withIntermediateDirectories:NO attributes:nil error:&error];
     }
     
     return self;
